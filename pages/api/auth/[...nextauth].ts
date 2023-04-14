2c17f60636876a78d65b25fe16b90a223dbe27f8
anantdhok <anantdhok16@gmail.com>
2023-04-14 15:47:20 +0530

    Add access-token in session response

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,5 +11,29 @@ export default NextAuth({
       issuer: process.env.AUTH_ISSUER
     })
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, user, token }) {
+      // Send properties to the client
+      // Like an access_token and user id from a provider.
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      // Persist the OAuth access_token and user id
+      // In the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+        if (profile) token.id = profile.sub;
+      }
+      return token;
+    }
+  },
   secret: process.env.JWT_SECRET
 });
