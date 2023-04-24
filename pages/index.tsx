ed9244b48d1cc7bb14245aa93ebd7651d3f8058f
anantdhok <anantdhok16@gmail.com>
2023-04-24 14:59:31 +0530

    Add loading while authenticatoin

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,11 @@ import Head from "next/head";
 import Router from "next/router";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
-  // const loading = status === "loading";
+  const { status, data: session } = useSession();
 
   useEffect(() => {
     if (session) Router.push("/profile");
-  }, [session]);
+  }, [status, session]);
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -21,40 +20,69 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="mb-2 text-3xl font-bold">Welcome to keyclaok 👋</h1>
-        <p className="mb-3 text-lg">Hi there, click on the button below to login.</p>
-        <button
-          className="my-2 w-[200px] rounded-full bg-blue-500 pt-1 pb-2 text-white"
-          onClick={e => {
-            e.preventDefault();
-            signIn("keycloak");
-          }}
-        >
-          Continue to sign-in
-        </button>
+        <h1 className="mb-2 text-3xl font-bold">Welcome 👋</h1>
+        {status === "unauthenticated" ? (
+          <div>
+            <p className="mb-3 text-lg">Authentication with keycloak.</p>
+            <button
+              className="my-2 w-[200px] rounded-full bg-blue-500 pt-1 pb-2 text-white"
+              onClick={e => {
+                e.preventDefault();
+                signIn("keycloak");
+              }}
+            >
+              Continue to sign-in
+            </button>
 
-        <p className="text-gray my-2 text-sm">Or, continue with</p>
-
-        <span className="flex flex-col">
-          <button
-            className="my-2 w-[200px] rounded-full bg-red-500 pt-1 pb-2 text-white"
-            onClick={e => {
-              e.preventDefault();
-              signIn("keycloak", {}, { kc_idp_hint: "google" });
-            }}
-          >
-            Google account
-          </button>
-          <button
-            className="my-2 w-[200px] rounded-full bg-red-500 pt-1 pb-2 text-white"
-            onClick={e => {
-              e.preventDefault();
-              signIn("keycloak", {}, { kc_idp_hint: "facebook" });
-            }}
-          >
-            Facebook account
-          </button>
-        </span>
+            <p className="text-gray my-2 text-sm">Or, continue with</p>
+            <span className="align-items-center flex justify-center">
+              <button
+                className="m-3"
+                onClick={e => {
+                  e.preventDefault();
+                  signIn("keycloak", {}, { kc_idp_hint: "google" });
+                }}
+              >
+                <img
+                  alt="Google"
+                  src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                  width={28}
+                  height={28}
+                />
+              </button>
+              <button
+                className="m-3"
+                onClick={e => {
+                  e.preventDefault();
+                  signIn("keycloak", {}, { kc_idp_hint: "facebook" });
+                }}
+              >
+                <img
+                  alt="Facebook"
+                  src="https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg"
+                  width={28}
+                  height={28}
+                />
+              </button>
+              <button
+                className="m-3"
+                onClick={e => {
+                  e.preventDefault();
+                  signIn("keycloak", {}, { kc_idp_hint: "microsoft" });
+                }}
+              >
+                <img
+                  alt="Microsoft"
+                  src="https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg"
+                  width={28}
+                  height={28}
+                />
+              </button>
+            </span>
+          </div>
+        ) : (
+          <p className="mb-3 text-lg">Please wait, preparing your session ...</p>
+        )}
       </main>
     </div>
   );
